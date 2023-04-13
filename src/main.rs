2a9d1266cf15235f17ea6ff95b88e7e2f1750495
Armando Salazar <armandosalazar351@gmail.com>
2023-04-12 19:23:07 -0600

    request works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let mut buffer: String = String::new();
+
+    // Con el expect evitamos el match de _result
+    std::io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line");
+
+
+    let price: Result<String, ureq::Error> = get_price(&buffer);
+    match price {
+        Ok(price) => println!("Price: {}", price),
+        Err(error) => println!("Error: {}", error)
+    }
+}
+
+// &str is a reference to a string
+// &String is a reference to a String
+fn get_price(cryptocurrency: &str) -> Result<String, ureq::Error> {
+    println!("Cryptocurrency: {}", cryptocurrency);
+    let url: String = format!("https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=usd", cryptocurrency);
+    let body: String = ureq::get(&url)
+        .call()?
+        .into_string()?;
+    Ok(body)
 }
